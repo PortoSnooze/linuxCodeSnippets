a29b53ecdea7ec0833819a34f28516eaf6655ca1
PortoSnooze <78099035+PortoSnooze@users.noreply.github.com>
2021-02-19 09:02:03 +0000

    Update mergeFilesSameHeader.sh

--- mergeFilesSameHeader.sh
+++ mergeFilesSameHeader.sh
@@ -4,6 +4,8 @@ head -1 file1.txt > head.txt && tail -n +2 file*.txt > main.txt && cat head.txt 
 
 # head -1:    writes header to separate file 
 # tail -n +2: output starting with the Kth line (combined with head -n prints all but the last K lines of each file)
-# cat: cat output files together 
+# cat: cat output files together (use paste if you want to merge 2 single-col files into 1 two-column file)
 # >> print all to final file 
 # && chains commands together. Next command is only run if the preceding command exited without errors (return code of 0)
+
+
