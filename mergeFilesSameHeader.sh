c0427549f6e4e2554860d8a01f03440748877ab4
PortoSnooze <78099035+PortoSnooze@users.noreply.github.com>
2021-02-19 08:51:21 +0000

    Create mergeFilesSameHeader.sh

---
+++ mergeFilesSameHeader.sh
@@ -0,0 +1,9 @@
+# how to merge multiple files with the same heading 
+
+head -1 file1.txt > head.txt && tail -n +2 file*.txt > main.txt && cat head.txt main.txt >> allMerged.txt
+
+# head -1:    writes header to separate file 
+# tail -n +2: output starting with the Kth line (combined with head -n prints all but the last K lines of each file)
+# cat: cat output files together 
+# >> print all to final file 
+# && chains commands together. Next command is only run if the preceding command exited without errors (return code of 0)
